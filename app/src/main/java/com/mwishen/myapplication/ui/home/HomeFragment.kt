40494d9a9e11c68fcb29278cc5f22b8595c83882
modcomlearning <modcomlearning@gmail.com>
2021-08-04 13:21:59 +0300

    Fourth Commit

--- app/src/main/java/com/mwishen/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mwishen/myapplication/ui/home/HomeFragment.kt
@@ -8,7 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+import com.mwishen.myapplication.Post
 import com.mwishen.myapplication.PostsAdapter
 import com.mwishen.myapplication.R
 import com.mwishen.myapplication.databinding.FragmentHomeBinding
@@ -39,11 +43,34 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         val root: View = binding.root
         //**********
-
+        //find the recycler in XML fragment_home.xml
+        recyclerPosts = root.findViewById(R.id.recyclerPosts)
+        loadPosts()//call load post function
         return root
-    }
+    }//oncreate ends here
+    private fun loadPosts() {
+        val query= FirebaseDatabase.getInstance().reference.child("MODCOM").child("POSTS")
+        val options = FirebaseRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+        //options has the data from firebase which have title , desc, image, timestamp
+        mAdapter = PostsAdapter(options) //pass data to the adapter
+        //since the adapter will only show one product, apply the adapter to
+        //recycler view so that it can loop all other products
+        recyclerPosts.apply {
+            //the recycler will display other products as a GRID of 2 columns
+            layoutManager = GridLayoutManager(activity,2)
+            adapter = mAdapter
+        }
+        //stop progress here
+    }//end load posts
+    override fun onStart() {
+        super.onStart()
+        mAdapter!!.startListening()
+    }//make it listen in real time
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
